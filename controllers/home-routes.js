d0d8272d56b695bf2912dc1d24cb23bd7d129961
Jessie Shockey <shockeyjessie92@gmail.com>
2023-09-30 13:08:38 -0600

    Create Blog Read Route

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,5 +1,32 @@
 const router = require('express').Router();
 const {Blog,Comment } = require('../models');
 
+//Get all Blog Posts and Comments
+router.get('/', async (req, res) => {
+    try {
+      const blogData = await Blog.findAll({
+        include: [
+          {
+            model: Comment
+          },
+        ],
+      });
+  
+    //   const galleries = dbGalleryData.map((gallery) =>
+    //     gallery.get({ plain: true })
+    //   );
+    //   res.render('homepage', {
+    //     galleries,
+    //     loggedIn: req.session.loggedIn,
+    //   });
+    res.status(200).json(blogData);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  });
+
+
+//login route needs added here
 
 module.exports = router;
