e9040ead36168a14bc9efd1d9258bc74e264370f
Jessie Shockey <shockeyjessie92@gmail.com>
2023-09-30 12:43:17 -0600

    Create User Create (POST) Route

---
+++ controllers/api/user-routes.js
@@ -0,0 +1,26 @@
+const router = require('express').Router();
+const { User } = require('../../models');
+
+// api/users
+
+// CREATE new user
+router.post('/', async (req, res) => {
+    try {
+      const dbUserData = await User.create({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password,
+      });
+  
+    //   req.session.save(() => {
+    //      req.session.loggedIn = true;
+    //      res.status(200).json(dbUserData);   
+    // });
+    res.status(200).json(dbUserData);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  });
+
+module.exports = router;
---
+++ controllers/home-routes.js
@@ -0,0 +1,5 @@
+const router = require('express').Router();
+const {Blog,Comment } = require('../models');
+
+
+module.exports = router;
