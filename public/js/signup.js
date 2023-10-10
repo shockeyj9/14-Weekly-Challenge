a93ef44fefd8ea7560f1fc054cce0ecd44354018
Jessie Shockey <shockeyjessie92@gmail.com>
2023-10-10 14:34:48 -0600

    Add username to User's model and handlebar's templates

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,7 +1,7 @@
 const signupFormHandler = async (event) => {
     event.preventDefault();
   
-    // const name = document.querySelector('#name-signup').value.trim();
+    const username = document.querySelector('#username-signup').value.trim();
     const email = document.querySelector('#email-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
     console.log(email, password)
@@ -9,7 +9,7 @@ const signupFormHandler = async (event) => {
       console.log('Post initiated')
       const response = await fetch('/api/users', {
         method: 'POST',
-        body: JSON.stringify({ /**name,*/ email, password }),
+        body: JSON.stringify({ username, email, password }),
         headers: { 'Content-Type': 'application/json' },
       });
   
