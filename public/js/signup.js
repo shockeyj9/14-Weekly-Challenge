a5e17af955e1fde528a89bab0b5f94681dccb6de
Jessie Shockey <shockeyjessie92@gmail.com>
2023-10-10 14:36:40 -0600

    Removed all console.logs

--- public/js/signup.js
+++ public/js/signup.js
@@ -4,9 +4,7 @@ const signupFormHandler = async (event) => {
     const username = document.querySelector('#username-signup').value.trim();
     const email = document.querySelector('#email-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
-    console.log(email, password)
-    if (/*name && */email && password) {
-      console.log('Post initiated')
+    if (username, email && password) {
       const response = await fetch('/api/users', {
         method: 'POST',
         body: JSON.stringify({ username, email, password }),
